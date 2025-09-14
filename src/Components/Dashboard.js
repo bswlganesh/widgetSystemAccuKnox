d0c9e815bcf4195621ed1af1a272e40b7f290018
Baswal Ganesh <baswalganesh1@gmail.com>
2025-09-14 12:28:06 +0530

    none

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -11,7 +11,7 @@ const Dashboard = () => {
   return (
     <div className="dashboard-container">
       <header className="dashboard-header">
-        <h1 className="dashboard-title">CNAPP Dashboard</h1>
+        <h1 className="dashboard-title">WIDGET DASHBOARD</h1>
         <div className="dashboard-controls">
           <button className="btn" onClick={() => setIsModalOpen(true)}>Add Widget +</button>
         </div>
